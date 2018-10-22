f91a9b2da9fabcc98acd650c45666a8163ed49e2
MiracleYoung <miracleyoung0723@gmail.com>
2018-10-22 23:09:06 +0800

    update

--- test/wx/test_filehelper.py
+++ test/wx/test_filehelper.py
@@ -4,39 +4,81 @@
 
 __author__ = 'MiracleYoung'
 
-import functools
+from functools import wraps, partial
+import logging
 
 
-def wraps(func):
-    @functools.wraps(func)
-    def _wrapper(*args, **kwargs):
-        ret = func(*args, **kwargs)
-
-        for item in args:
-            print(item.__name__)
-        return ret
-
-    return _wrapper
+# Utility decorator to attach a function as an attribute of obj
+def attach_wrapper(obj, func=None):
+    if func is None:
+        return partial(attach_wrapper, obj)
+    setattr(obj, func.__name__, func)
+    return func
 
 
-class A:
-    def __init__(self):
-        self.meta = {
-            'a': {
-                'aa': 1
-            }
-        }
+def logged(level, name=None, message=None):
+    '''
+    Add logging to a function. level is the logging
+    level, name is the logger name, and message is the
+    log message. If name and message aren't specified,
+    they default to the function's module and name.
+    '''
 
-    @wraps
-    def go(self, v):
-        self.meta['b'] = v
+    def decorate(func):
+        logname = name if name else func.__module__
+        log = logging.getLogger(logname)
+        logmsg = message if message else func.__name__
 
-import inspect
-def get_current_function_name():
-    return inspect.stack()[1][3]
-class MyClass:
-    def function_one(self):
-        print("%s.%s invoked"%(self.__class__.__name__, get_current_function_name()))
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            log.log(level, logmsg)
+            return func(*args, **kwargs)
+
+        # Attach setter functions
+        @attach_wrapper(wrapper)
+        def set_level(newlevel):
+            nonlocal level
+            level = newlevel
+
+        @attach_wrapper(wrapper)
+        def set_message(newmsg):
+            nonlocal logmsg
+            logmsg = newmsg
+
+        return wrapper
+
+    return decorate
+
+
+# Example use
+@logged(logging.DEBUG)
+def add(x, y):
+    return x + y
+
+
+@logged(logging.CRITICAL, 'example')
+def spam():
+    print('Spam!')
+
+
 if __name__ == "__main__":
-    myclass = MyClass()
-    myclass.function_one()
+    # import logging
+    #     #
+    #     # logging.basicConfig(level=logging.DEBUG)
+    #     # # add(2, 3)
+    #     # add.set_message('Add called')
+    #     # add(2, 3)
+
+    import asyncio
+
+
+    async def fn(s):
+        print('fn start')
+        await asyncio.sleep(s)
+        print('fn stop')
+
+
+    loop = asyncio.get_event_loop()
+    tasks = [fn(3), ]
+    loop.run_until_complete(asyncio.wait(tasks))
+
--- wx/filehelper.py
+++ wx/filehelper.py
@@ -6,73 +6,118 @@ __author__ = 'MiracleYoung'
 
 __all__ = ['FileHelper']
 
-import functools, inspect
+import functools, random, time, threading
 
-from itchat import send_msg
+import asyncio
 
 from etc import *
 
 
-def _reset_status(func):
-    @functools.wraps(func)
-    def wrap(*args, **kwargs):
-        ret = func(*args, **kwargs)
-        for item in args[0]._meta.values():
-            for k, v in item.items():
-                if k.startswith('is'):
-                    v = False
-        return ret
-
-    return wrap
-
-
 class FileHelper:
+    _usage = '''
+    '''
+    _own = {
+        'NickName': 'filehelper',
+        'UserName': 'filehelper',
+    }
+
     def __init__(self, instance):
-        self._meta = {
-            'default': {
-                'is_default': False,
-                'res_text': '默认消息',
-            },
-            'mass_text': {
-                'is_mass_text': False,
-                'res_text': '请发送群发内容',
-            },
-            'mass_article': {
-                'is_mass_article': False,
-                'res_text': '请发送文章链接',
-            }
-
-        }
-        self._usage = '''
-        
         '''
-        self._unlimited_group = []
+        _meta = {
+            'obj':{ # 消息发送对象
+                'ul': [], # unlimit group
+                'l': [], # limit group
+            },
+            'reply':{
+                'text': '',
+                'article': '',
+            }
+        }
+        '''
+        self._meta = {
+            'action': {
+                'mass': False,
+                'friend': False,
+            },
+            'obj': {
+                'ul': [],
+                'l': [],
+            },
+            'reply': {
+                'text': '请输入要群发的消息',
+                'article': '请输入要群发的文章链接',
+            },
+            'tmp': ['测试群', '测试群2']
+        }
         self._instance = instance
+        self._current_cmd = None
+        self._th_update = threading.Thread(target=self._update_meta, args=(), daemon=True)
+        self._th_update.start()
 
-        for group in self._instance.get_chatrooms():
-            if group.NickName:
-                self._unlimited_group.append(group.UserName)
+    def _update_meta(self):
+        '''
+        初始化限时推送的群组
+        '''
 
+        def _filter_groups(self, group):
+            for limit in LIMIT_GROUP:
+                if limit in group:
+                    return True
+            return False
 
+        while True:
+            print('30s')
+            _all = self._instance.get_chatrooms()
+            _limits = filter(_filter_groups, _all)
+            for limit in _limits:
+                self._meta['obj']['ul'].append(limit.UserName)
+            # 初始化无限制推送的群组
+            self._meta['obj']['l'].extend(list(set(_all) - set(self._meta['obj']['ul'])))
+            time.sleep(30)
 
-    def _send_msg(self, cmd):
-        _cmd = '_'.join(cmd.split())
-        _item = self._meta.get(_cmd, 'default')
-        if _item.get(f'is_{_cmd}', 'is_default'):
-            return self._instance._send_msg(msg=_item.get('res_text', ''), toUserName=to_user)
-        else:
-            _item[f'is_{_cmd}'] = True
+    def _register_msg(func):
+        @functools.wraps(func)
+        def decorator(self, msg, *args, **kwargs):
+            _action, _reply, _obj = func.__name__.split('_')
+            if self._meta['action'][_action]:
+                _to_user = self._meta['obj'][_obj]
+                for _group in _to_user:
+                    self.send_msg(msg, _group)
+                    time.sleep(random.randrange(0, 20))
+                self._meta['action'][_action] = False
+                self._current_cmd = None
+                self.send_msg('群发消息发送完毕', self._own['UserName'])
+            else:
+                self._meta['action'][_action] = True
+                self._current_cmd = func.__name__
+                self.send_msg(self._meta['reply'][_reply], self._own['UserName'])
+            return
+
+        return decorator
+
+    @_register_msg
+    def mass_text_ul(self, msg=None):
+        pass
+
+    @_register_msg
+    def mass_text_l(self, msg):
+        pass
+
+    @_register_msg
+    def mass_article_ul(self, msg):
+        pass
+
+    @_register_msg
+    def mass_article_l(self, msg):
+        pass
+
+    def send_msg(self, msg, to_user):
+        self._instance.send_msg(msg, to_user)
 
     @property
     def usage(self):
         return self._usage
 
-    def mass_article(self, to_user):
-        self._send_msg(to_user)
-
-    def mass_text(self, cmd):
-        self._send_msg(cmd)
-
-    @_reset_status
-    def exec(self, cmd):
-        eval(cmd)(cmd)
+    @property
+    def current_cmd(self):
+        return self._current_cmd
